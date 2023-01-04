4832503ffe24615bcc6e81de5034c08b64b7eb56
Léo Boyer <contact@leoboyer.fr>
2023-01-04 14:45:50 +0100

    Biblio type icon

--- components/Biblio/BiblioType.vue
+++ components/Biblio/BiblioType.vue
@@ -4,5 +4,16 @@ defineProps<{
 }>()
 </script>
 <template>
- <div class="flex items-center bg-gray-200 rounded text-xs py-0.5 px-1">{{ type }}</div>
+ <div class="flex items-center rounded text-xs py-0.5 px-1 gap-1"
+ :class="{
+   'bg-red-200': type === 'Vidéo',
+   'bg-cyan-100': type === 'Article',
+   'bg-purple-200': type === 'Article universitaire',
+ }"
+ >
+   <Icon v-if="type === 'Vidéo'" name="uil:youtube" />
+   <Icon v-else-if="type === 'Article'" name="uil:newspaper" />
+   <Icon v-else-if="type === 'Article universitaire'" name="uil:file-search-alt" />
+   <span>{{ type }}</span>
+ </div>
 </template>
